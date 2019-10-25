fb8b0a47cc6dde3c4881a491e3612b574e53402e
timottaff <timothytaffarel@gmail.com>
2019-10-25 15:46:05 +0700

    Revert home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="home">
     <h1>This is an Home page</h1>
-    <v-btn text small>Normal</v-btn>
-    <v-tabs>
-    <v-tab>Item One</v-tab>
-    <v-tab>Item Two</v-tab>
-    <v-tab>Item Three</v-tab>
-    <v-tab>Item Four</v-tab>
-    </v-tabs>
   </div>
   
 </template>
